d92a6bb8ebfa39c1a9f20f8d749ef2523b9e7ce4
Dinush Chathurya <38061436+dinushchathurya@users.noreply.github.com>
2019-05-19 07:59:17 +0530

    Update hospital.sql

--- Database/hospital.sql
+++ Database/hospital.sql
@@ -34,17 +34,6 @@ CREATE TABLE `admin` (
   `password` varchar(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`adminID`, `username`, `password`) VALUES
-(1, 'hasindu', 'hasindu'),
-(2, 'admin', 'admin'),
-(3, 'dinush', 'dinush'),
-(4, 'akash', 'akash'),
-(5, 'akila', 'akila');
-
 -- --------------------------------------------------------
 
 --
@@ -57,20 +46,6 @@ CREATE TABLE `appointment` (
   `pName` varchar(15) NOT NULL,
   `room` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `appointment`
---
-
-INSERT INTO `appointment` (`apoinmentID`, `dName`, `pName`, `room`) VALUES
-(1, 'Sajeeb', 'Shafayat Jamil', 987),
-(2, 'Sifat', 'Sajeeb Shahriar', 806),
-(3, 'akash', 'Akash', 15),
-(4, 'akash', 'Akash', 15),
-(5, 'akash', 'Akash', 15),
-(6, 'akash', 'Akash', 15),
-(7, 'haren', 'Akash', 15),
-(8, 'akash', 'Akash', 15);
 
 -- --------------------------------------------------------
 
@@ -95,14 +70,6 @@ CREATE TABLE `doctor` (
   `password` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `doctor`
---
-
-INSERT INTO `doctor` (`count`, `date`, `name`, `age`, `gender`, `blood`, `dept`, `phone`, `email`, `status`, `address`, `room`, `username`, `password`) VALUES
-(13, '21-10-2018', 'Akash', 21, 'Male', 'AB+', 'Medecine', '0774630527', 'akashmanujaya@gmail.com', 'Single', 'Kalutara', 15, 'akash', 'akash16785'),
-(14, '21-11-2018', 'Haren Tharushan', 30, 'Male', 'A+', 'Medecine', '+944673528', 'haren@gmail.com', 'Single', 'Kalutara', 15, 'haren', 'haren');
-
 -- --------------------------------------------------------
 
 --
@@ -121,16 +88,6 @@ CREATE TABLE `patient` (
   `disease` varchar(20) NOT NULL,
   `room` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `patient`
---
-
-INSERT INTO `patient` (`count`, `date`, `name`, `age`, `gender`, `address`, `phone`, `status`, `disease`, `room`) VALUES
-(13, '21-12-2018', 'Akash', 20, 'Male', 'Kalutara', '+94774630527', 'Single', 'Cold', 68),
-(15, '5-12-2018', 'Hasindu', 21, 'Male', 'Moratuwa', '+94774685978', 'Single', 'Headache', 12),
-(16, '21-8-2018', 'Kevin', 16, 'Male', 'kalutara', '+94774652956', 'Single', 'Heart Attack', 16),
-(17, '8-12-2018', 'Akila', 21, 'Male', 'Horana', '+94774630528', 'Single', 'Cancer', 8);
 
 -- --------------------------------------------------------
 
@@ -152,13 +109,6 @@ CREATE TABLE `receptionist` (
   `username` varchar(20) NOT NULL,
   `password` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `receptionist`
---
-
-INSERT INTO `receptionist` (`count`, `joining`, `name`, `age`, `gender`, `blood`, `email`, `phone`, `address`, `status`, `username`, `password`) VALUES
-(11, '21-10-2018', 'Akash', 20, 'Male', 'AB+', 'akashmanujaya@gmail.com', '0342221726', 'Kalutara', 'Single', 'akash', 'akash');
 
 --
 -- Indexes for dumped tables
